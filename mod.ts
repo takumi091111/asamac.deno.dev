3e22d346e8cf77f319d20c73d465809d648aa3f5
takumi091111 <takumi091111@gmail.com>
2021-03-31 01:17:31 +0900

    fix charset

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 addEventListener('fetch', (event) => {
   const response = new Response('🍔', {
-    headers: { 'content-type': 'text/plain' },
+    headers: { 'content-type': 'text/plain; charset=UTF-8' },
   });
   event.respondWith(response);
 });
